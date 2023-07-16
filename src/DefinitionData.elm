90c60466a05546c7ec1cf1016de6a55f4e5dcff8
Ryan1729 <Ryan1729@gmail.com>
2023-07-16 01:06:00 -0600

    more definitions

--- src/DefinitionData.elm
+++ src/DefinitionData.elm
@@ -51,4 +51,42 @@ all =
       , definition = "A large set of Japanese symbols"
       , mayIndicate = "a menu option to switch symbols when entering names."
       }
+    , { hirigana = "もどる"
+      , katakana = notApplicable
+      , includesKanji = "戻る"
+      , romanji = "modoru"
+      , definition = "To go back"
+      , mayIndicate = "the delete/backspace option when entering text, or option to return to the previous menu"
+      }
+    , { hirigana = "おわり"
+      , katakana = notApplicable
+      , includesKanji = "終わり"
+      , romanji = "owari"
+      , definition = "close, end, ending"
+      , mayIndicate = "the submit option when entering text"
+      }
+    , { hirigana = "せいべつ\u{3000}は"
+      , katakana = notApplicable
+      , includesKanji = "性別 は"
+      , romanji = "seibetsu ha"
+      , definition = "gender and/or sex"
+      , mayIndicate = "that a choice of player gender and/or sex is being presented"
+      }
+    , { hirigana = "おとこ"
+      , katakana = notApplicable
+      , includesKanji = "男"
+      , romanji = "otoko"
+      , definition = "man/male"
+      , mayIndicate = "that an option to select for a character to be of masculine gender and/or male sex is presented"
+      }
+    , { hirigana = "おんな"
+      , katakana = notApplicable
+      , includesKanji = "女"
+      , romanji = "onna"
+      , definition = "woman/female"
+      , mayIndicate = "that an option to select for a character to be of feminine gender and/or female sex is presented"
+      }
+
+    -- TODO add something like エックスジェンダ if we actually observe it in a game.
+    -- A different way to say the same thing may be what is actually used in practice.
     ]
